6f419a9383795cb1402d3e6fefe4ff33bb8bedac
Uzair-U <129953055+Uzair-U@users.noreply.github.com>
2023-05-04 11:47:36 +0200

    Add files via upload

---
+++ semantic.py
@@ -0,0 +1,36 @@
+import spacy
+nlp = spacy.load('en_core_web_md')
+
+word1 = nlp("cat")
+word2 = nlp("monkey")
+word3 = nlp("banana")
+
+print(word1.similarity(word2))
+print(word3.similarity(word2))
+print(word3.similarity(word1))
+
+tokens = nlp('cat apple monkey banana lion ')
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
+
+sentence_to_compare = "Why is my cat on the car"
+
+sentences = ["where did my dog go",
+"Hello, there is my car",
+"I've lost my car in my car",
+"I'd like my boat back",
+"I will name my dog Diana"]
+
+model_sentence = nlp(sentence_to_compare)
+
+for sentence in sentences:
+    similarity = nlp(sentence).similarity(model_sentence)
+    print(sentence + " - ", similarity)
+
+'''I noticed a few things with the cat monkey apple banana similarity. I noticed:
+The feature sees apple and banana as a higher similarity compared to monkey and cat. Despite both being animals, 
+they are different type of animals and the feature notices this and accounts for it too. I also thought Monkey and banana
+would have a higher similarity compared to monkey and cat..
+I also noticed that lion and monkey have a higher similarity than lion and cat... which seems strange!'''
+
